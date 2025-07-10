5a7effcbd535edbe77b32d3e45bde8b4a94a77b4
Anjalireddy888 <saumiinturi@gmail.com>
2025-07-10 21:53:31 +0530

    sort array in asc order

---
+++ SortArrAsc.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class SortArrAsc {
+    public static void main(String[] args){
+        int[] nums = {8,9,7,4,7,1,9};
+        Arrays.sort(nums); //default asc
+        System.out.println(Arrays.toString(nums));
+    }
+}
