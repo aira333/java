4cb04a60557164b97a75adab22f3fbdc9d0c0f5c
Anjalireddy888 <saumiinturi@gmail.com>
2025-07-11 19:51:02 +0530

    calling static and non static methods

--- callingMethod.java
+++ callingMethod.java
@@ -1,5 +1,5 @@
 public class callingMethod {
-    // staic methods belongs to the class, can call them directly using the class name
+    // static methods belongs to the class, can call them directly using the class name
     public static int add(int a, int b) {
         return a + b;
     }
@@ -11,4 +11,25 @@ public class callingMethod {
         int sum2 = callingMethod.add(100, 25);
         System.out.println("100 + 25 = " + sum2);
     }
+
 }
+
+// non-static methods belong to an object of a class. create an object forst and then call method on it
+
+// public class Dog {
+//     String name;
+
+//     public Dog(String name) {
+//         this.name = name;
+//     }
+
+//     public void bark() {
+//         System.out.println(name + "woof!");
+//     }
+
+//     public static void main (String[] args) {
+//         Dog myDog = new Dog("Buddy");
+//         System.out.println("My dog");
+//         myDog.bark();
+//     }
+// }
