4e972af6dd68059f5a8bceb28aa3f925989e2645
Anjalireddy888 <saumiinturi@gmail.com>
2025-07-20 19:12:24 +0530

    armstrong number

---
+++ armstrongnumber.java
@@ -0,0 +1,32 @@
+public class armstrongnumber {
+    public static boolean isArmstrong(int number){
+        if (number < 0) {
+            return false;
+        }
+        if (number == 0){
+            return false;
+        }
+
+        int originalnum = number;
+        int sumOfPowers = 0;
+        int numberofDigits = 0;
+
+        int tempNumber = number;
+        while (tempNumber != 0) {
+            tempNumber /= 10;
+            numberofDigits++;
+        }
+
+        tempNumber = originalnum;
+        while (tempNumber != 0) {
+            int digit = tempNumber % 10;
+            sumOfPowers += Math.pow(digit, numberofDigits);
+            tempNumber /= 10;
+        }
+
+        return sumOfPowers == originalnum;
+    }
+    public static void main(String[] args) {
+        System.out.println(isArmstrong(9));
+    }
+}
