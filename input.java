45bd61571945278d84d45d48107b95b9d031de29
Anjalireddy888 <saumiinturi@gmail.com>
2025-07-26 20:47:36 +0530

    print sum of twwo numbers

---
+++ input.java
@@ -0,0 +1,11 @@
+public class input {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 9;
+
+        int sum = (a+b);
+
+        System.out.println("the sum of two numbers is " + sum);
+    }
+    
+}
