3eaf7eb2730fbf6077a3a1915c5962bfcc12a640
Anjalireddy888 <saumiinturi@gmail.com>
2025-07-28 03:51:14 +0530

    reverse an array

---
+++ reverseArray.java
@@ -0,0 +1,8 @@
+public class reverseArray {
+    public static void main(String[] args) {
+        int [] arr = {1,3,5,6,7};
+        for (int i = arr.length - 1; i >=0; i--){
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
